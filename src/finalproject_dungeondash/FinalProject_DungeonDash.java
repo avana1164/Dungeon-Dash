09abc493fa7c4326d5ae5ce2438131aa80443225
AAnand05 <44912634+fligmiger@users.noreply.github.com>
2022-06-17 18:03:34 -0400

    added game

---
+++ src/finalproject_dungeondash/FinalProject_DungeonDash.java
@@ -0,0 +1,27 @@
+/*
+Avinash Anand 
+ */
+package finalproject_dungeondash;
+
+import GUIs.LoginWindow;
+import java.io.File;
+import java.io.PrintWriter;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author jason
+ */
+public class FinalProject_DungeonDash extends JFrame {
+// instantiat all nessisary variables 
+    public static LoginWindow login;   
+    public static void main(String[] args) {
+// create a new jframe window  
+        
+        login = new LoginWindow();// create a new login window
+        login.setVisible(true);// set the window to visible 
+        
+        
+    }
+
+}
